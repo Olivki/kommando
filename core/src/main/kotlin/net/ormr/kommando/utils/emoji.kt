19f51d4ed35d04d94b1bfa81d12586fd9fc04016
Oliver Berg <oliver@berg.moe>
2022-04-05 18:57:01 +0200

    Add extension functions for working with emojis for components

---
+++ core/src/main/kotlin/net/ormr/kommando/utils/emoji.kt
@@ -0,0 +1,55 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 Oliver Berg
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package net.ormr.kommando.utils
+
+import dev.kord.common.entity.DiscordPartialEmoji
+import dev.kord.common.entity.optional.optional
+import dev.kord.core.builder.components.emoji
+import dev.kord.core.entity.GuildEmoji
+import dev.kord.core.entity.ReactionEmoji
+import dev.kord.rest.builder.component.ButtonBuilder
+import dev.kord.rest.builder.component.SelectOptionBuilder
+import dev.kord.x.emoji.DiscordEmoji
+import dev.kord.x.emoji.toReaction
+
+public fun ButtonBuilder.emoji(emoji: DiscordEmoji) {
+    emoji(emoji.toReaction())
+}
+
+public fun SelectOptionBuilder.emoji(emoji: GuildEmoji) {
+    this.emoji = DiscordPartialEmoji(id = emoji.id, name = null, animated = emoji.isAnimated.optional())
+}
+
+public fun SelectOptionBuilder.emoji(emoji: ReactionEmoji.Unicode) {
+    this.emoji = DiscordPartialEmoji(name = emoji.name, id = null)
+}
+
+public fun SelectOptionBuilder.emoji(emoji: ReactionEmoji.Custom) {
+    this.emoji = DiscordPartialEmoji(name = emoji.name, id = emoji.id, animated = emoji.isAnimated.optional())
+}
+
+public fun SelectOptionBuilder.emoji(emoji: DiscordEmoji) {
+    emoji(emoji.toReaction())
+}
