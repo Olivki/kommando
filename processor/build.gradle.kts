cbd1af672fbb047f43de1f7a3de3fc45ee64785d
Oliver Berg <oliver@berg.moe>
2022-04-12 21:30:27 +0200

    Update KSP to 1.6.20-1.0.5

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -30,7 +30,7 @@ version = "0.3.0"
 
 dependencies {
     implementation("com.squareup:kotlinpoet-ksp:1.11.0")
-    implementation("com.google.devtools.ksp:symbol-processing-api:1.6.20-1.0.4")
+    implementation("com.google.devtools.ksp:symbol-processing-api:1.6.20-1.0.5")
     implementation(project(":core"))
     kapt("com.google.auto.service:auto-service:1.0.1")
     compileOnly("com.google.auto.service:auto-service:1.0.1")
