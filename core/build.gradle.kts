a8e7e17b2351862770d88c089fa36d6ff483924b
Oliver Berg <oliver@berg.moe>
2022-05-10 14:45:44 +0200

    Update `kord` to 0.8.0-M13

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -27,7 +27,7 @@ version = "0.0.5"
 dependencies {
     api(kotlin("reflect"))
     api("org.kodein.di:kodein-di:7.11.0")
-    api("dev.kord:kord-core:0.8.0-M12")
+    api("dev.kord:kord-core:0.8.0-M13")
     api("dev.kord.x:emoji:0.5.0")
     implementation("io.github.reactivecircus.cache4k:cache4k:0.5.0")
 
