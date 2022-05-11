52e7a69e5dc54797227cba19206c09be9d2e6222
Oliver Berg <oliver@berg.moe>
2022-05-11 19:04:05 +0200

    Use latest version of `core`

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -26,12 +26,12 @@ plugins {
     kotlin("kapt")
 }
 
-version = "0.8.0"
+version = "0.9.0"
 
 dependencies {
     implementation("com.squareup:kotlinpoet-ksp:1.11.0")
     implementation("com.google.devtools.ksp:symbol-processing-api:1.6.21-1.0.5")
-    implementation("net.ormr.kommando:core:0.0.9")
+    implementation("net.ormr.kommando:core:0.0.13")
     kapt("com.google.auto.service:auto-service:1.0.1")
     compileOnly("com.google.auto.service:auto-service:1.0.1")
 }
